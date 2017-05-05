46148ad517f29b3b2b7b5f021c18247ea7a82472
AJ Saclayan <ajsaclayan@gmail.com>
2017-05-05 16:57:39 -0400

    Broken tests

--- test/unit/app/femr/business/services/MockTest.java
+++ test/unit/app/femr/business/services/MockTest.java
@@ -8,6 +8,7 @@ import femr.common.models.PatientItem;
 import femr.data.DataModelMapper;
 import femr.data.daos.system.PatientRepository;
 import femr.data.models.mysql.Patient;
+import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -15,11 +16,10 @@ import org.mockito.Mockito;
 
 import java.util.Date;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 /**
@@ -45,15 +45,12 @@ public class MockTest {
             //initialize mocks
             initMocks(this);
 
-        //initialize service with mock objects
-        patientService = new PatientService(mockPatientRepository, mockDataModelMapper, mockItemModelMapper);
+            //initialize service with mock objects
+            patientService = new PatientService(mockPatientRepository, mockDataModelMapper, mockItemModelMapper);
 
-        //create random patient
-        patient = new Patient();
-
-        //re-define mock method calls
-        when(mockPatientRepository.retrievePatientById(2)).thenReturn(patient);
-        when(mockPatientRepository.savePatient(patient)).thenReturn(patient);
+            //re-define mock method calls (stubs)
+            when(mockPatientRepository.retrievePatientById(2)).thenReturn(patient);
+            when(mockPatientRepository.savePatient(patient)).thenReturn(patient);
         }
 
     @Test
@@ -65,13 +62,57 @@ public class MockTest {
         //act
         response = patientService.deletePatient(1, 2, null);
 
-        //assert
-        verify(mockPatientRepository).retrievePatientById(1);
-        //verify(mockPatientRepository.savePatient(patient));
-        //        assertFalse(mockPatientRepository.retrievePatientByIdWasCalled);
-//        assertFalse(mockPatientRepository.savePatientWasCalled);
+        //verify
+        verify(mockPatientRepository, never()).retrievePatientById(1);
+        verify(mockPatientRepository, never()).savePatient(patient);
         assertTrue(response.hasErrors());
     }
 
+    @Test
+    public void deletePatient_emptyReasonProvided_errorProduced() throws Exception {
 
+        //arrange
+        ServiceResponse<PatientItem> response;
+
+        //act
+        response = patientService.deletePatient(1, 2, "");
+
+        //verify
+        verify(mockPatientRepository, never()).retrievePatientById(1);
+        verify(mockPatientRepository, never()).savePatient(patient);
+        //assert
+        assertTrue(response.hasErrors());
+    }
+
+    @Test
+    public void deletePatient_parametersProvided_parametersSaved() throws Exception {
+        Patient mockPatient = Mockito.mock(Patient.class);
+        mockPatient.setIsDeleted(DateTime.now());
+        mockPatient.setDeletedByUserId(2);
+        mockPatient.setReasonDeleted("fake reason");
+        when(mockPatientRepository.savePatient(patient)).thenReturn(mockPatient);
+
+
+        //To-do-
+        // Should we mock patient ?
+
+        //arrange
+        ServiceResponse<PatientItem> response;
+
+//        when(mockPatientRepository.savePatient(mockPatient).set
+        //act
+        response = patientService.deletePatient(1, 2, "fake reason");
+
+        //verify
+        verify(mockPatientRepository).retrievePatientById(1);
+        verify(mockPatientRepository).savePatient(mockPatient);
+        //make sure patient repository was called with proper reason
+        //assert
+        assertEquals(mockPatientRepository.savePatient(mockPatient).getReasonDeleted(), "fake reason");
+        //make sure proper user ID is logged
+        assertEquals(mockPatientRepository.savePatient(mockPatient).getDeletedByUserId(), (Integer)2);
+        //assertEquals(mockPatientRepository.mockPatient.getId(), 1);
+
+        assertFalse(response.hasErrors());
+    }
 }
